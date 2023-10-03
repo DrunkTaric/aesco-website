c46fa84dc0866585b18a4406a1e117374fe8470f
DrunkTaric <comborus.init@proton.me>
2023-10-03 17:47:29 +0300

    removing lazy loading and optimization from these images

--- app/products/components/product.tsx
+++ app/products/components/product.tsx
@@ -1,7 +1,5 @@
 "use client"
 
-import Image from "next/image"
-
 export interface ProductProps {
     id: number
     name: string
@@ -16,7 +14,7 @@ export default function Product(props: ProductProps) {
         <div className="pt-20 pl-7">
             <div className="w-[17rem] h-[20rem] border-2 border-white rounded-xl bg-white">
                 <div className="h-[15rem] overflow-hidden">
-                    <Image className="block w-[70%] h-[98%] pt-8 mr-auto ml-auto bg-center bg-cover" src={props.image} width={120} height={120} alt={props.name}/>
+                    <img className="block w-[70%] h-[98%] pt-8 mr-auto ml-auto bg-center bg-cover" src={props.image} alt={props.name}/>
                 </div>
                 <h1 className="p-3 text-center text-black font-bold">{props.name}</h1>
             </div>
